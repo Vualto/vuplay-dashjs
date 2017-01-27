fcd293d588f86961a9c65e954d04ea87d19c69c4
Robert Mitchell <robert.mitchell@vualto.com>
2017-01-27 14:32:39 +0000

    copy other files task added

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,10 +2,19 @@ module.exports = function(grunt) {
     grunt.initConfig({
         package: grunt.file.readJSON('package.json'),
 
-        clean: ['dist/*']
+        clean: ['dist/*'],
+        copy: {
+            nonJs: {
+                expand: true,
+                src: ['index.html', 'poster.png'],
+                dest: 'dist/',
+                flatten: true
+            }
+        }
     });
 
     grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
 
-    grunt.registerTask('default', ['clean']);
+    grunt.registerTask('build', ['clean', 'copy']);
 };
