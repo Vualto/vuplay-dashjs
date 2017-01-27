9e1eb0a9882ad7a7bc39423eeee05fe1199419fc
Robert Mitchell <robert.mitchell@vualto.com>
2017-01-27 15:07:25 +0000

    add simple dev server

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,20 +1,35 @@
 module.exports = function(grunt) {
     grunt.initConfig({
+        dist: 'dist',
         package: grunt.file.readJSON('package.json'),
 
-        clean: ['dist/*'],
+        clean: ['<%= dist %>/*'],
         copy: {
-            nonJs: {
+            other: {
                 expand: true,
                 src: ['index.html', 'poster.png'],
-                dest: 'dist/',
+                dest: '<%= dist %>/',
                 flatten: true
+            }
+        },
+
+        connect: {
+            server: {
+                options: {
+                    protocol: 'http',
+                    hostname: 'dashjs.vuplay.local.drm.technology',
+                    port: 14703,
+                    base: 'dist',
+                    keepalive: true
+                }
             }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-connect');
 
     grunt.registerTask('build', ['clean', 'copy']);
+    grunt.registerTask('serve', ['build', 'connect']);
 };
