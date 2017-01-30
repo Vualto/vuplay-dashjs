4020e76a4334e87aac2fd1d0a12460ebeb9d2f67
Robert Mitchell <robert.mitchell@vualto.com>
2017-01-30 16:18:04 +0000

    uglify the vuplay stuff

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,12 @@
 module.exports = function (grunt) {
     var dashjsUrls = {
-        release: {
-            min: "http://cdn.dashjs.org/latest/dash.all.min.js",
-            debug: "http://cdn.dashjs.org/latest/dash.all.debug.js"
-        }
+        min: "http://cdn.dashjs.org/latest/dash.all.min.js",
+        debug: "http://cdn.dashjs.org/latest/dash.all.debug.js"
+    };
+
+    var vuplayUrls = {
+        min: "vuplay.min.js",
+        debug: "vuplay.js"
     };
 
     grunt.initConfig({
@@ -26,6 +29,13 @@ module.exports = function (grunt) {
                 dest: "dist/vuplay.js",
             },
         },
+        uglify: {
+            js: {
+                files: {
+                    "dist/vuplay.min.js": ['dist/vuplay.js']
+                }
+            }
+        },
         "string-replace": {
             dist: {
                 files: [
@@ -38,7 +48,11 @@ module.exports = function (grunt) {
                     replacements: [
                         {
                             pattern: "{dashjs}",
-                            replacement: grunt.option("debug") ? dashjsUrls.release.debug : dashjsUrls.release.min
+                            replacement: grunt.option("debug") ? dashjsUrls.debug : dashjsUrls.min
+                        },
+                        {
+                            pattern: "{vuplayjs}",
+                            replacement: grunt.option("debug") ? vuplayUrls.debug : vuplayUrls.min
                         }
                     ]
                 }
@@ -61,9 +75,10 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks("grunt-contrib-clean");
     grunt.loadNpmTasks("grunt-contrib-copy");
     grunt.loadNpmTasks("grunt-contrib-concat");
+    grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks("grunt-string-replace");
     grunt.loadNpmTasks("grunt-contrib-connect");
 
-    grunt.registerTask("build", ["clean", "copy", "concat", "string-replace"]);
+    grunt.registerTask("build", ["clean", "copy", "concat", "uglify", "string-replace"]);
     grunt.registerTask("serve", ["build", "connect"]);
 };
