b666794b343a19e15b9865ba3cb3406a4d0ce2ee
Robert Mitchell <robert.mitchell@vualto.com>
2017-01-30 11:53:04 +0000

    add dashjs urls and option to use debug mode

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,35 +1,61 @@
 module.exports = function(grunt) {
-    grunt.initConfig({
-        dist: 'dist',
-        package: grunt.file.readJSON('package.json'),
+    var dashjsUrls = {
+        release: {
+            min: "http://cdn.dashjs.org/latest/dash.all.min.js",
+            debug: "http://cdn.dashjs.org/latest/dash.all.debug.js"
+        }
+    };
 
-        clean: ['<%= dist %>/*'],
+    grunt.initConfig({
+        dist: "dist",
+        package: grunt.file.readJSON("package.json"),
+
+        clean: ["<%= dist %>/*"],
         copy: {
             other: {
                 expand: true,
-                src: ['index.html', 'poster.png'],
-                dest: '<%= dist %>/',
+                src: ["index.html", "poster.png"],
+                dest: "<%= dist %>/",
                 flatten: true
+            }
+        },
+        "string-replace": {
+            dist: {
+                files: [
+                    {
+                        src: "dist/index.html",
+                        dest: "dist/index.html"
+                    }
+                ],
+                options: {
+                    replacements: [
+                        {
+                            pattern: "{dashjs}",
+                            replacement: grunt.option("debug") ? dashjsUrls.release.debug : dashjsUrls.release.min
+                        }
+                    ]
+                }
             }
         },
 
         connect: {
             server: {
                 options: {
-                    protocol: 'http',
-                    hostname: 'dashjs.vuplay.local.drm.technology',
+                    protocol: "http",
+                    hostname: "dashjs.vuplay.local.drm.technology",
                     port: 14703,
-                    base: 'dist',
+                    base: "dist",
                     keepalive: true
                 }
             }
         }
     });
 
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks("grunt-contrib-clean");
+    grunt.loadNpmTasks("grunt-contrib-copy");
+    grunt.loadNpmTasks("grunt-string-replace");
+    grunt.loadNpmTasks("grunt-contrib-connect");
 
-    grunt.registerTask('build', ['clean', 'copy']);
-    grunt.registerTask('serve', ['build', 'connect']);
+    grunt.registerTask("build", ["clean", "copy", "string-replace"]);
+    grunt.registerTask("serve", ["build", "connect"]);
 };
