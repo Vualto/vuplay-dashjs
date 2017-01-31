e3432d27f7a3cd54138b84139870129d5b372eca
rob-vualto <robert.mitchell@vualto.com>
2017-01-31 09:21:18 +0000

    modificatons to get keyid

--- src/OverrideKeySystemWidevine.js
+++ src/OverrideKeySystemWidevine.js
@@ -1,5 +1,12 @@
 OverrideKeySystemWidevine = function () {
+    var context = this.context;
+    var parent = this.parent;
+    
     return {
+        getInitData: function (cp, keyId) {
+            return CommonEncryption.parseInitDataFromContentProtection(cp);
+        },
+
         getLicenseRequestFromMessage: function (message) {
             body = {
                 'token': "vudrm-token",
--- src/OverrideProtectionKeyController.js
+++ src/OverrideProtectionKeyController.js
@@ -1,9 +1,15 @@
 OverrideProtectionKeyController = function () {
+    var parent = this.parent;
+
     return {
         getSupportedKeySystemsFromContentProtection: function(cps) {
             var cp, ks, ksIdx, cpIdx;
             var supportedKS = [];
-
+            var keySystems = parent.getKeySystems();
+            var cpsWithKeyId = cps.find(function (element) {
+                return element.KID !== null;
+            });
+            
             if (cps) {
                 for (ksIdx = 0; ksIdx < keySystems.length; ++ksIdx) {
                     ks = keySystems[ksIdx];
@@ -12,7 +18,7 @@ OverrideProtectionKeyController = function () {
                         if (cp.schemeIdUri.toLowerCase() === ks.schemeIdURI) {
 
                             // Look for DRM-specific ContentProtection
-                            var initData = ks.getInitData(cp);
+                            var initData = ks.getInitData(cp, cpsWithKeyId.KID);
                             if (!!initData) {
                                 supportedKS.push({
                                     ks: keySystems[ksIdx],
